969263f036bb272e3f19b9fd5f0a824def0d18af
Peter Isza <peter.isza@gmail.com>
2016-01-23 14:38:00 +0100

    Fix return value of ECG compressor functions.

--- Software/Common/DataFormat/DifferenceEcgCompressor.cpp
+++ Software/Common/DataFormat/DifferenceEcgCompressor.cpp
@@ -15,29 +15,37 @@ bool DifferenceEcgCompressor::putSample(const int* channels) {
 	for(unsigned int i = 0; i < numChannels; ++i) {
 		int diff = channels[i] - ecgPredictor.getPrediction(i);
 		//printf(" newdata=%d pred=%d\n", channels[i], ecgPredictor.getPrediction(i));
+		bool okay = true;
 		if(diff >= smallMin && diff <= smallMax) {
-			bitStream.pushBits(0, 1);
-			bitStream.pushBits(diff, smallBitNum);
+			okay &= bitStream.pushBits(0, 1);
+			okay &= bitStream.pushBits(diff, smallBitNum);
 			//printf("S %d\n", diff);
 		} else {
-			bitStream.pushBits(1, 1);
-			bitStream.pushBits(channels[i], fullBitNum);
+			okay &= bitStream.pushBits(1, 1);
+			okay &= bitStream.pushBits(channels[i], fullBitNum);
 			//printf("F %d\n", diff);
 		}
+		if(!okay)
+			return false;
 	}
 	ecgPredictor.putSample(channels);
+	return true;
 }
 
 bool DifferenceEcgCompressor::getSample(int *channels) {
 	for(unsigned int i = 0; i < numChannels; ++i) {
 		int full = bitStream.popBits(1);
 		//printf('');
+		int bitNum = full ? fullBitNum : smallBitNum;
+		if(bitStream.getFreeBits() < bitNum)
+			return false;
 		if(full)
 			channels[i] = bitStream.popBitsSigned(fullBitNum);
 		else
 			channels[i] = ecgPredictor.getPrediction(i) + bitStream.popBitsSigned(smallBitNum);
 	}	
 	ecgPredictor.putSample(channels);
+	return true;
 }
 
 void DifferenceEcgCompressor::setNumChannels(unsigned pNumChannels){
